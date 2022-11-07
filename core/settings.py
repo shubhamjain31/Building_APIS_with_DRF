274dddb18b02d16af4236c7ad89c1f28c4cf72b9
shubhamjain31 <sj27754@gmail.com>
2022-11-07 22:26:12 +0530

    create database and connect with mysql

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     'blog',
+        'USER':     'root',
+        'PASSWORD': 'root',
+        'HOST':     'localhost',
+        'PORT':     '3306',
     }
 }
 
