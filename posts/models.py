fe88a806e8ecb323fa17f9b1c73cc2a219c65f9b
shubhamjain31 <sj27754@gmail.com>
2022-11-09 22:06:38 +0530

    created model of post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class Post(models.Model):
+    title           = models.CharField(max_length=50)
+    content         = models.TextField()
+    created         = models.DateTimeField(auto_now_add=True)
+    author          = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-created"]
